1b5f8504737ba642e1ba757aa49af98bedc5a5ac
AsteyaLaxmanan <68880458+AsteyaLaxmanan@users.noreply.github.com>
2021-05-24 17:07:33 -0700

    Add files via upload

---
+++ cube.js
@@ -0,0 +1,32 @@
+(
+    function() {
+        "use strinct";
+        function e (e,t,o) {
+            function a () {
+                for(
+                    var e = Math.cos,
+                    a = [
+                        new THREE.Vector3(1,1,1),
+                        new THREE.Vector3(1,1,-1),
+                        new THREE.Vector3(-1,1,-1),
+                        new THREE.Vector3(-1,1,1),
+                        new THREE.Vector3(1,-1,1),
+                        new THREE.Vector3(1,-1,-1),
+                        new THREE.Vector3(-1,-1,-1),
+                        new THREE.Vector3(-1,-1,1),
+
+                    ],
+                    s = 0;
+                    8>s;
+                    s=s+1
+                )
+                C.push([]),
+                L.push([]),
+                
+
+            }
+
+        }
+
+    }
+)
